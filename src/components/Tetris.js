42e62a91f518fe82e78a2f7d6b42d186b37db6fe
Marcus Christensen <marcus.christensen@devbrains.se>
2020-10-27 20:36:18 +0100

    Styling the components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,19 +6,22 @@ import { createStage } from '../gameHelpers'
 import Display from './Display'
 import Stage from './Stage'
 import StartButton from './StartButton'
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris'
 
 const Tetris = () => (
-  <div>
-    <Stage stage={createStage()} />
-    <aside>
-      <div>
-        <Display text="Score" />
-        <Display text="Rows" />
-        <Display text="Level" />
-      </div>
-      <StartButton />
-    </aside>
-  </div>
+  <StyledTetrisWrapper>
+    <StyledTetris>
+      <Stage stage={createStage()} />
+      <aside>
+        <div>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
+        </div>
+        <StartButton />
+      </aside>
+    </StyledTetris>
+  </StyledTetrisWrapper>
 )
 
 export default Tetris
